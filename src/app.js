609c71cfbc4e6ef044472f35b45b18baa1f23164
Lautaro <107657857+tatidee@users.noreply.github.com>
2024-05-22 19:39:02 +0000

    Generador de excusas en Javascript

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Write your code here
+  let p = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRandomExcuse() {
+    var randomWho = who[Math.floor(Math.random() * who.length)];
+    var randomAction = action[Math.floor(Math.random() * action.length)];
+    var randomWhat = what[Math.floor(Math.random() * what.length)];
+    var randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  }
+
+  p.textContent = getRandomExcuse();
 };
